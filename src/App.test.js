9ad03c5b99867c1f5738f2a65efc170dba296a45
BrandonC123 <brandoc318@gmail.com>
2022-07-11 19:58:42 -0700

    Test shop component including rendering items and route to designated item page.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
+import ItemPage from "./components/ItemPage";
+import Shop from "./components/Shop";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Shop component", () => {
+    it("Properly renders item via ViewItems component", () => {
+        render(<Shop />, { wrapper: MemoryRouter });
+        expect(
+            screen.getByRole("heading", { name: "Sunglass 1" }).textContent
+        ).toMatch("Sunglass 1");
+    });
+    it("Renders all items", () => {
+        render(<Shop />, { wrapper: MemoryRouter });
+        expect(screen.getAllByRole("img").length).toBe(12);
+    });
+    it("Item page properly displays", () => {
+        render(<Shop />, { wrapper: MemoryRouter });
+        const itemLink = screen.getByText("Sunglass 1");
+        userEvent.click(itemLink);
+        expect(screen.getByText("Sunglass 1").textContent).toMatch(
+            "Sunglass 1"
+        );
+    });
 });
